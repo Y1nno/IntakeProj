c747274c0e3042b27f3fe802725820c41e97c202
Y1nno <59004110+Y1nno@users.noreply.github.com>
2026-01-11 19:52:18 -0600

    commit

--- Assets/Scripts/Network/NetworkRunnerHandler.cs
+++ Assets/Scripts/Network/NetworkRunnerHandler.cs
@@ -8,23 +8,37 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Collections;
 
-public class NetworkRunnerHandler : MonoBehaviour
+public class NetworkRunnerHandler : MonoBehaviour, INetworkRunnerCallbacks
 {
     public NetworkRunner networkRunnerPrefab;
 
     NetworkRunner networkRunner;
+    bool isStarting;
+    bool hasStarted;
 
-    void Start()
+    SessionLobby sessionLobby = SessionLobby.Shared;
+
+    async void Start()
     {
         networkRunner = Instantiate(networkRunnerPrefab);
         networkRunner.name = "NetworkRunner";
+        networkRunner.ProvideInput = true;
+        networkRunner.AddCallbacks(this);
 
-        var clientTask = InitializeNetworkRunner(networkRunner, GameMode.AutoHostOrClient, NetAddress.Any(), SceneManager.GetActiveScene().buildIndex, null);
-        
-        Debug.Log("NetworkRunner started"); 
+        EnsureSceneManager(networkRunner);
+
+        try
+        {
+            await networkRunner.JoinSessionLobby(sessionLobby);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogWarning($"Failed to join lobby. Starting host instead. Error: {ex.Message}");
+            await StartHost();
+        }
     }
 
-    protected virtual Task InitializeNetworkRunner(NetworkRunner runner, GameMode gameMode, NetAddress address, SceneRef scene, Action<NetworkRunner> initialized)
+    void EnsureSceneManager(NetworkRunner runner)
     {
         var SceneManager = runner.GetComponent<NetworkSceneManagerDefault>();
 
@@ -32,20 +46,131 @@ public class NetworkRunnerHandler : MonoBehaviour
         {
             SceneManager = runner.gameObject.AddComponent<NetworkSceneManagerDefault>();
         }
-        runner.ProvideInput = true;
-
-        var clientTask = runner.StartGame(new StartGameArgs()
-        {
-            GameMode = gameMode,
-            Address = address,
-            Scene = scene,
-            SessionName = "Test",
-            Initialized = initialized,
-            SceneManager = SceneManager,
-
-        });
-
-        return clientTask;
     }
 
+    async Task StartHost()
+    {
+        if (isStarting || hasStarted)
+        {
+            return;
+        }
+        isStarting = true;
+
+        await networkRunner.StartGame(new StartGameArgs()
+        {
+            GameMode = GameMode.Host,
+            Address = NetAddress.Any(),
+            SessionName = "Test",
+            Scene = SceneManager.GetActiveScene().buildIndex,
+            SceneManager = networkRunner.GetComponent<NetworkSceneManagerDefault>(),
+        });
+
+        hasStarted = true;
+        isStarting = false;
+        Debug.Log("Host started");
+    }
+
+    async Task StartClient()
+    {
+        if (isStarting || hasStarted)
+        {
+            return;
+        }
+
+        isStarting = true;
+
+        await networkRunner.StartGame(new StartGameArgs()
+        {
+            GameMode = GameMode.Client,
+            Address = NetAddress.Any(),
+            Scene = SceneManager.GetActiveScene().buildIndex,
+            SessionName = "Test",
+            SceneManager = networkRunner.GetComponent<NetworkSceneManagerDefault>(),
+        });
+
+        hasStarted = true;
+        isStarting = false;
+        Debug.Log("Client started");
+    }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+        if (hasStarted || isStarting)
+        {
+            return;
+        }
+
+        var hasSession = sessionList.Any(session => session.Name == "Test");
+        if (hasSession)
+        {
+            _ = StartClient();
+        }
+        else
+        {
+            _ = StartHost();
+        }
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+    }
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+    }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason reason)
+    {
+        hasStarted = false;
+        isStarting = false;
+    }
+
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+    }
+
+    public void OnDisconnectedFromServer(NetworkRunner runner)
+    {
+    }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {
+    }
+
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+    }
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+    }
+
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+    }
+
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+    }
+
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
+    {
+    }
+
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+    }
+
+    public void OnSceneLoadStart(NetworkRunner runner)
+    {
+    }
 }
+
